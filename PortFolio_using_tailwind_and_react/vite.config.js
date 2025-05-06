966f65b6a085af057123d7c3ffa0c8b06a524fd1
bharath-padega <padegabharathkumar@gmail.com>
2025-05-06 02:49:43 -0500

    Fixed alignment and git error

---
+++ PortFolio_using_tailwind_and_react/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/PortFolio_using_tailwind_and_react/', // <- match your repo name here
+});
