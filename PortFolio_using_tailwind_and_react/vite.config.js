4581eadf5ffb3448d204849c18d6e1f44da98f8f
bharath-padega <padegabharathkumar@gmail.com>
2025-05-10 03:21:57 -0500

    Fixing directory

--- PortFolio_using_tailwind_and_react/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/PortFolio_using_tailwind_and_react/', // <- match your repo name here
-});
